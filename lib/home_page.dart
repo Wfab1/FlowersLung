bc4936687fd4454a3b2e1e04bcf77da82ad256c1
Wanessa <wfab1@aluno.ifal.edu.br>
2025-05-26 16:03:52 -0300

    Tela do Menu quase concluída (falta colocar a imagem dos pulmões como o fundo do AppBar)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 
 class _HomePageState extends State<HomePage> {
   @override
@@ -13,15 +17,50 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFFf4eedd),
+        body: buildContent(),
       ),
     );
   }
 
+
+  Widget buildContent() {
+    return Center(
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-              ),
+          Text(
+            'Flowers Lung',
+            style: GoogleFonts.lobster(
+              color: Colors.black,
+              fontSize: 70,
             ),
+          ),
+          SizedBox(),
+          Image.asset(
+            'assets/images/PulmaoComFlores.png',
+            width: 320,
+          ),
+          SizedBox(),
+          ElevatedButton(
+            onPressed: () {
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFFD7AE5E),
+              padding:EdgeInsets.symmetric(horizontal: 40, vertical: 14),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              elevation: 3,
+            ),
+            child: Text('Entrar', style: GoogleFonts.openSans(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+            ),
+            ),
+          ),
         ],
+      ),
     );
   }
 }
---
+++ lib/menu_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
+  @override
+  State<MenuPage> createState() => _MenuPageState();
+}
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFd3b360),
+        appBar: buildAppBar(),
+        body: buildListView(),
+      ),
+    );
+  }
+
+  buildAppBar() {
+    return AppBar(
+      toolbarHeight: 100,
+      actions: [
+        Container(
+          //child: Image.asset('assets/images/FundoBegePulmao.jpg', width: 100, height: 100,),
+          child: Image.asset('assets/images/PulmaoComFlores.png', width: 80),
+        ),
+      ],
+      centerTitle: false,
+      backgroundColor: Color(0xFFf4eedd),
+      title: Text(
+        'Flowers Lung',
+        style: GoogleFonts.lobster(
+          color: Color(0xFFE1A100),
+          fontSize: 40,
+        ),
+      ),
+    );
+  }
+
+  buildListView(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Image.asset('assets/images/IconPulmao.png', width: 80),
+              decoration: BoxDecoration(
+                  color: Color(0xFFf4eedd),
+                  borderRadius: BorderRadius.circular(16)
+              ),
+            ),
+            Text('Informações', style: GoogleFonts.openSans(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
+        SizedBox(height: 24),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 64,
+          children: [
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: Image.asset('assets/images/IconJogar.png', width: 80),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFf4eedd),
+                      borderRadius: BorderRadius.circular(16)
+                  ),
+                ),
+                Text('Jogar', style: GoogleFonts.openSans(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: Image.asset('assets/images/IconComunidade.png', width: 80),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFf4eedd),
+                      borderRadius: BorderRadius.circular(16)
+                  ),
+                ),
+                Text('Comunidade', style: GoogleFonts.openSans(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700),
+                ),
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
