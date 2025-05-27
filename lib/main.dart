2ab266042eee40f2263456e5e3da4aec082a5564
beatriz <abo4@aluno.ifal.edu.br>
2025-05-27 11:35:16 -0300

    Criação da tela de info dos sisntomas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:flowerslung/infoSintomas_page.dart';
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(),)
+  runApp(MaterialApp(home: InfoSintomasPage(),)
   );
-
-
 }
