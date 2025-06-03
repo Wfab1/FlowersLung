8074ea99e2297e888c898295a3a9729144e11d97
Francyele17 <fds3@aluno.ifal.edu.br>
2025-06-03 17:08:49 -0300

    Criação da tela de MenuInfo.

--- lib/menuInfo_page.dart
+++ lib/menuInfo_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class MenuInfoPage extends StatefulWidget {
   const MenuInfoPage({super.key});
   @override
   State<MenuInfoPage> createState() => _MenuInfoPageState();
 }
+
 class _MenuInfoPageState extends State<MenuInfoPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/images/FundoPulmaoVermelho.jpg'))
+          image: DecorationImage(
+            image: AssetImage('assets/images/FundoPulmaoVermelho.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
@@ -23,12 +26,13 @@ class _MenuInfoPageState extends State<MenuInfoPage> {
       ),
     );
   }
+
   buildAppBar() {
     return AppBar(
       toolbarHeight: 100,
       actions: [
         Container(
-         child: Image.asset('assets/images/PulmaoComFlores.png', width: 50),
+          child: Image.asset('assets/images/PulmaoComFlores.png', width: 50),
         ),
       ],
       centerTitle: true,
@@ -44,9 +48,10 @@ class _MenuInfoPageState extends State<MenuInfoPage> {
     );
   }
 
-  buildListView(){
+  buildListView() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Column(
           children: [
@@ -56,107 +61,96 @@ class _MenuInfoPageState extends State<MenuInfoPage> {
               height: 70,
               padding: EdgeInsets.all(8),
               margin: EdgeInsets.all(8),
-              child:Text('Pulmão', style: GoogleFonts.adamina(
+              child: Text(
+                'Pulmão',
+                style: GoogleFonts.adamina(
                   color: Color(0xFFF4EEDD),
                   fontSize: 32,
-                  fontWeight: FontWeight.w700),
+                  fontWeight: FontWeight.w700,
+                ),
               ),
               decoration: BoxDecoration(
-                  color: Color(0xFFa54d3b),
-                  borderRadius: BorderRadius.circular(16),
+                color: Color(0xFFa54d3b),
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 4),
+        Container(
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(8),
+          child: Text(
+            'Sintomas',
+            style: GoogleFonts.adamina(
+              color: Color(0xFFF4EEDD),
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xFFa54d3b),
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
 
-              ),
+        //SizedBox(height: 4),
+        Container(
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(8),
+          child: Text(
+            'Tratamento',
+            style: GoogleFonts.adamina(
+              color: Color(0xFFF4EEDD),
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
             ),
-          ],
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xFFa54d3b),
+            borderRadius: BorderRadius.circular(16),
+          ),
         ),
         SizedBox(height: 4),
-        Column(
-          children: [
-            SizedBox(height: 4),
-            Container(
-              width: 900,
-              height: 70,
-              padding: EdgeInsets.all(8),
-              margin: EdgeInsets.all(8),
-              child:Text('Sintomas', style: GoogleFonts.adamina(
-                  color: Color(0xFFF4EEDD),
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700),
-              ),
-              decoration: BoxDecoration(
-                  color: Color(0xFFa54d3b),
-                  borderRadius: BorderRadius.circular(16)
-              ),
+        Container(
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(8),
+          child: Text(
+            'Apoio',
+            style: GoogleFonts.adamina(
+              color: Color(0xFFF4EEDD),
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
             ),
-          ],
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xFFa54d3b),
+            borderRadius: BorderRadius.circular(16),
+          ),
         ),
-        SizedBox(height: 4),
-        Column(
-          children: [
-            SizedBox(height: 4),
-            Container(
-              width: 900,
-              height: 70,
-              padding: EdgeInsets.all(8),
-              margin: EdgeInsets.all(8),
-              child:Text('Tratamento', style: GoogleFonts.adamina(
-                  color: Color(0xFFF4EEDD),
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700),
-              ),
-              decoration: BoxDecoration(
+        SizedBox(height: 200),
+        Padding(
+          padding: EdgeInsets.only(bottom: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 14),
+                decoration: BoxDecoration(
                   color: Color(0xFFa54d3b),
-                  borderRadius: BorderRadius.circular(16)
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 4),
-        Column(
-          children: [
-            SizedBox(height: 4),
-            Container(
-              width: 900,
-              height: 70,
-              padding: EdgeInsets.all(8),
-              margin: EdgeInsets.all(8),
-              child:Text('Apoio', style: GoogleFonts.adamina(
-                  color: Color(0xFFF4EEDD),
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700),
-              ),
-              decoration: BoxDecoration(
-                  color: Color(0xFFa54d3b),
-                  borderRadius: BorderRadius.circular(16)
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 120),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Column(
-              children: [
-                SizedBox(height: 4),
-                Container(
-                  width: 100,
-                  height: 60,
-                  padding: EdgeInsets.all(8),
-                  margin: EdgeInsets.all(8),
-                  child:Text('Sair', style: GoogleFonts.adamina(
-                      color: Color(0xFFF4EEDD),
-                      fontSize: 32,
-                      fontWeight: FontWeight.w700),
-                  ),
-                  decoration: BoxDecoration(
-                      color: Color(0xFFa54d3b),
-                      borderRadius: BorderRadius.circular(16)
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  'Sair',
+                  style: GoogleFonts.openSans(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ],
     );
