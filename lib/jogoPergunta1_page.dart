5c35c6432cc395f45ae1be5e85478a526d2a216a
Wfab1 <wfab1@aluno.ifal.edu.br>
2025-06-11 18:23:30 -0300

    Merge branch 'vitoria' into wanessa

--- lib/jogoPergunta1_page.dart
+++ lib/jogoPergunta1_page.dart
@@ -23,7 +23,7 @@ class _JogoPergunta1Page extends State<JogoPergunta1Page> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: buildListView(10, 30, 0, 10, 20),
+        body: buildListView(15, 30, 0, 10, 20),
       ),
     );
   }
