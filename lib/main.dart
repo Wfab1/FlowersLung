8a85c101e3cecd8379ba49450570ebf6253f8c69
Wanessa <wfab1@aluno.ifal.edu.br>
2025-06-11 14:29:49 -0300

    Telas do jogo (perguntas 23 a 30) prontas!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flowerslung/infoTratamento_page.dart';
+import 'package:flowerslung/jogoPergunta26_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: InfoTratamentoPage(),)
+  runApp(MaterialApp(home: JogoPergunta26Page(),)
   );
 }
