c4af9754c37877b76426e6fa4fcacd759160f705
Francyele <fds3@aluno.ifal.edu.br>
2025-08-20 08:31:08 -0300

    Teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flowerslung/InfoApoio_page.dart';
+import 'package:flowerslung/pulmao_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: InfoApoioPage(),)
+  runApp(MaterialApp(home: PulmaoPage(),)
   );
 }
